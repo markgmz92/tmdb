56a710cf79d0a328f752bbf2da7d51cb1f5374e8
markgmz92 <markgmz92@gmail.com>
2022-08-15 13:41:51 +0800

    [TMDB - 011] Footer done

---
+++ src/component/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BsFacebook, BsInstagram } from 'react-icons/bs';
+
+function Footer() {
+  return (
+    <div className='w-full my-auto py-16 px-24 md:px-56 grid lg:grid-cols-1  text-gray-300 bg-[#032541] '>
+      <div className='grid lg:grid-cols-1'>
+        <h1 className='w-full text-3xl flex justify-center text-[#00df9a]'>
+          TMDB.
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
